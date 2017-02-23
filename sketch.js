b5ea34a702d02374115fdb97f8d69b9ba74d85f4
Thorben <thorben.schaefer@student.fh-kiel.de>
2017-02-23 18:10:25 +0100

    First  lib add

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var circleX = 0;
+var offset = 0;
+function setup() {
+    createCanvas(600, 400);
+}
+
+function draw() {
+
+   background(0);
+    strokeWeight(4);
+    stroke(255);
+    for(var x = 0; x <= mouseX; x +=50){  
+      fill(random(255),random(255),random(255));
+      ellipse(x, 200,25,25);
+    }
+offset = offset +1;
+}
